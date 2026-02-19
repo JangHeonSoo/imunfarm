be48c29d2e62721e61ec15ba53f896f2b733b233
JangHeonSoo <denosinfo@gmail.com>
2026-02-19 17:31:42 +0900

    Fix: Resolve RSS feed build error by fixing import paths

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,7 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 import { siteConfig } from '@/site-config'
-import { getPostUrl } from '@/utils/post'
-import { DEFAULT_LOCALE } from '@/utils/locale'
+import { getPostUrl, DEFAULT_LOCALE } from '@/utils'
 
 export async function get() {
 	const posts = await getCollection('blog')
